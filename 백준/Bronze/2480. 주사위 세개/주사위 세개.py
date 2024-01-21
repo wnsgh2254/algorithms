28eeea1c59c8959f4224336bba2b7b42f71e67c8
wnsgh2254 <70765479+wnsgh2254@users.noreply.github.com>
2024-01-22 01:27:34 +1100

    [Bronze IV] Title: 주사위 세개, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -3,12 +3,10 @@ input = sys.stdin.readline
 
 a,b,c, = map(int, input().split())
 
-if a == b and a==c:
+if a == b == c:
     print(10000+(a*1000))
-elif a== b :
+elif a== b or a == c:
     print(1000+a*100)
-elif a==c:
-    print(1000 + a * 100)
 elif b==c:
     print(1000 + b * 100)
 else:
