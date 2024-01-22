3311bb99d22f89dfa2a94fc5c694407b8b0ba591
wnsgh2254 <70765479+wnsgh2254@users.noreply.github.com>
2024-01-22 22:12:56 +1100

    [Bronze V] Title: 개수 세기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -4,10 +4,7 @@ input = sys.stdin.readline
 n = int(input())
 nums = list(map(int, input().split()))
 v = int(input())
-result = 0
 
-for i in nums:
-    if i == v:
-        result+=1
-print(result)
+print(nums.count(v))
+
 
