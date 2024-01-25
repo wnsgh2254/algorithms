e2556e9614828094217575c3484c9d0460738985
wnsgh2254 <70765479+wnsgh2254@users.noreply.github.com>
2024-01-25 16:50:31 +1100

    [Silver V] Title: 분수찾기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1193. 분수찾기/분수찾기.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+num = int(input())
+line = 1
+
+while num > line:
+    num -= line
+    line += 1
+
+
+if line % 2 == 0:
+    a = num
+    b = line - num + 1
+
+
+elif line % 2 == 1:
+    a = line - num + 1
+    b = num
+
+print(f'{a}/{b}')
