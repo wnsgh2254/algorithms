a7d407dd14742b5ce0205f9e847f6a936450f19f
wnsgh2254 <70765479+wnsgh2254@users.noreply.github.com>
2024-01-25 18:40:14 +1100

    [Bronze I] Title: 소인수분해, Time: 1688 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+i = 2
+while n != 1:
+    if n == 1:
+        break
+    if n % i == 0:
+        print(i)
+        n = n // i
+    else:
+        i+=1
