7746de1b9c0753462eb5849d24a878318950b444
wnsgh2254 <70765479+wnsgh2254@users.noreply.github.com>
2024-01-25 17:53:17 +1100

    [Bronze III] Title: 약수 구하기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+lst = []
+for i in range(1, N+1) :
+    if N % i == 0 :
+        lst.append(i)
+
+if len(lst) < K :	
+    print(0)
+else :
+    print(lst[K-1])
